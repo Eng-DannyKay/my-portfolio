d81aac3f84354f14164bf9f49f5654a312d5566f
Daniel Forson <danielkojoforson27@gmail.com>
2026-01-17 17:29:19 +0000

    feat: remove withCredentials from axios instance configuration

--- src/lib/features/axios.ts
+++ src/lib/features/axios.ts
@@ -4,10 +4,8 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axios = axiosClient.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
+    "Content-Type": "application/json"
   }
 });
 
